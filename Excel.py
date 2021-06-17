66e132a48ec34b3bb59bea19c5573b6920aa5003
Stian Furu <stian.furu97@gmail.com>
2021-06-17 16:11:40 +0200

    Tester pyxl

--- Excel.py
+++ Excel.py
@@ -1,4 +1,14 @@
-print('hallo ja')
+from openpyxl import Workbook 
 
+filename = "testbook.xlsx"
 
-#test123
+workbook = Workbook()
+sheet = workbook.active
+
+sheet["A1"] = "Test123"
+
+sheet["D4"] = "Test43215321"
+
+workbook.save(filename=filename)
+
+#tester pyxl. lager en ny excel fil og skirver inn i sheet
