88bcc42d5bf53079d5819cf9fcb85f34a0e044e9
Stian Furu <stian.furu97@gmail.com>
2021-06-18 14:51:53 +0200

    skriver resultat til md

--- Excel.py
+++ Excel.py
@@ -3,4 +3,6 @@ import pandas as pd
 data = pd.read_excel('Diku.xlsx', sheet_name='DIKU')
 
 bool_series = pd.isnull(data["Emneansvarlig"])
-print(data[bool_series])
+
+md = open("resultat.md", "w+")
+md.write(str(data))
