7a82a3676f90585e8a72442959693c41dd257a56
Stian Furu <stian.furu97@gmail.com>
2021-06-18 11:05:18 +0200

    la til DIKU excel i repositoriet

--- Excel.py
+++ Excel.py
@@ -1,14 +1,2 @@
 from openpyxl import Workbook 
 
-filename = "testbook.xlsx"
-
-workbook = Workbook()
-sheet = workbook.active
-
-sheet["A1"] = "Test123"
-
-sheet["F3"] = 'enda en test'
-
-workbook.save(filename=filename)
-
-#tester pyxl. lager en ny excel fil og skirver inn i sheet
