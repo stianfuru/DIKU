65526241a4a22bdcd4b0736823aff6ab4edcd15d
Stian Furu <stian.furu97@gmail.com>
2021-06-21 13:35:05 +0200

    Versjon av wordsearch

--- Excel.py
+++ Excel.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 df = pd.read_excel('Diku.xlsx', sheet_name='DIKU', usecols='B,C,O,P,Q')
+df.dropna(inplace = True)
 
 
-keywords = ['fluidstatikk', 'programmering']
+keywords = ['fluidstatikk', 'programmering', 'bygningsfysiske']
 
-output =  df.query('"fluidstatikk" in "Læringsutbytte - Kunnskap"')
+output =  df['Læringsutbytte - Kunnskap'].str.contains(keywords[0])
+df_drop = df[output].drop('Læringsutbytte - Kunnskap', axis=1)
+df_drop1 = df_drop.drop('Læringsutbytte - Ferdigheter', axis=1)
+df_final = df_drop1.drop('Læringsutbytte - Generell Kompetanse', axis=1)
+print(df_final)
 
-print(output)
-
-#md = open("resultat.md", "w+")
-#md.write(str(df))
+md = open("resultat.md", "w+")
+md.write(str(df_final))
--- resultat.md
+++ resultat.md
@@ -1,6 +1,2 @@
-    Emnekode                    Emnenavn Emneansvarlig
-10  BYPE2700            Prosjektstryring           NaN
-26       NaN                         NaN           NaN
-27       NaN                         NaN           NaN
-40  FEPE2100                  Statistikk           NaN
-47  EMPE3200  Prosjektledelse og økonomi           NaN
+   Emnekode         Emnenavn
+1  BYPE1500  Fysikk og kjemi
