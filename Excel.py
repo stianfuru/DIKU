3afae51522679fba791e48982fbc95279d330f5e
Stian Furu <stian.furu97@gmail.com>
2021-06-22 11:57:14 +0200

    søkefunksjon som gir ut emnekode

--- Excel.py
+++ Excel.py
@@ -1,15 +1,14 @@
-from os import sep
 import pandas as pd
 import nltk
-import sklearn 
 import string
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 
 df = pd.read_excel('Diku.xlsx', sheet_name='DIKU', usecols='B,C,O,P,Q')
 df.dropna(inplace = True)
 
 kolonner = ['Læringsutbytte - Kunnskap','Læringsutbytte - Ferdigheter','Læringsutbytte - Generell Kompetanse']
-keywords = ['fluidstatistik', 'programmering', 'bygningsfysiske']
+keywords = ['fluidstatikk', 'betong', 'matematikk']
 
 def text_process(frame):
     nopunc = [char for char in frame if char not in string.punctuation]
@@ -22,11 +21,34 @@ LUK = df['Læringsutbytte - Kunnskap'].apply(text_process)
 LUF = df['Læringsutbytte - Ferdigheter'].apply(text_process)
 LUG = df['Læringsutbytte - Generell Kompetanse'].apply(text_process)
 
-print(LUK)
+Emnekode = df['Emnekode']
 
-#her har kamalan kommentert 
-#test 2 - push
 
+def wordsearch(frame):
+    j = 0
+    k = 0
+    i = 0
+    unique = 0
+    words = 0
+    for _ in keywords:
+        
+        for _ in frame:
+            bow_transformer = CountVectorizer(analyzer=text_process).fit(frame[i])
+            unique += (len(bow_transformer.vocabulary_))
+            for _ in frame[i]:
+                if keywords[k] == frame[i][j]:    
+                    print(frame[i][j]+' '+ Emnekode[i])
+                j = j + 1
+            j = 0
+            i = i + 1
+        i = 0
+        k = k + 1
+
+
+wordsearch(LUG) 
+#print(unique)
+#print(words)
+#print(LUK[0][0])
 #df_final = df.drop(kolonner, axis=1)
 
 #print(df.groupby('Læringsutbytte - Kunnskap').describe())
